074044da5312264026bf66a14792d7760e37d2ec
anand parmar <devparmar967@gmail.com>
2022-11-12 16:54:55 +0530

    added yaml environment file

--- utils/datasets/IAM/reorder_wordsdir.py
+++ utils/datasets/IAM/reorder_wordsdir.py
@@ -7,5 +7,5 @@ from CONF import *
 def reorder():
     if host != "localhost":
         for subdir in os.listdir(image_dir):
-            shutil.move(f'{subdir}/*', f'{image_dir}')
+            shutil.move(f'{image_dir}/{subdir}/*', f'{image_dir}')
             os.rmdir(subdir)
